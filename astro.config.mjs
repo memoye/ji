ed7d588126fd711afa3a7cdd7041b1e11fbd9e55
Brown <memoyebrown@gmail.com>
2025-03-15 15:34:04 +0100

    update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jessicaiwuala.vercel.app",
+  site: "https://iwualajessica.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
